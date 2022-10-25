daa884fa5828051294253024ce6ecf299e742408
Muhammad Sameer Bin Raees <44906406+sameerbinraees@users.noreply.github.com>
2022-10-25 21:38:28 +0500

    first commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,22 @@
+
+import Link from 'next/link'
+import { PrismicProvider } from '@prismicio/react'
+import { PrismicPreview } from '@prismicio/next'
+import { repositoryName } from '../prismicio'
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+export default function App({ Component, pageProps }) {
+  return (
+    <PrismicProvider
+      internalLinkComponent={({ href, ...props }) => (
+        <Link href={href}>
+          <a {...props} />
+        </Link>
+      )}
+    >
+      <PrismicPreview repositoryName={repositoryName}>
+        <Component {...pageProps} />
+      </PrismicPreview>
+    </PrismicProvider>
+  )
 }
-
-export default MyApp
